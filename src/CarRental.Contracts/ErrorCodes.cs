8f70001575d85927704563920ee68dbf31cac27b
vallmar <markus_vall@hotmail.com>
2026-09-20 19:25:46 +0200

    Remove obsolete duplicate-booking API error code

--- src/CarRental.Contracts/ErrorCodes.cs
+++ src/CarRental.Contracts/ErrorCodes.cs
@@ -10,7 +10,6 @@ public static class ErrorCodes
     public const string AuthenticationInvalidCredentials = "AUTH_INVALID_CREDENTIALS";
     public const string AuthenticationRequired = "AUTH_REQUIRED";
     public const string PickupInvalidInput = "PICKUP_INVALID_INPUT";
-    public const string PickupBookingAlreadyExists = "PICKUP_BOOKING_ALREADY_EXISTS";
     public const string ReturnInvalidInput = "RETURN_INVALID_INPUT";
     public const string ReturnRentalNotFound = "RETURN_RENTAL_NOT_FOUND";
     public const string InternalError = "INTERNAL_ERROR";
