deb60886d3326eb4d6142dd35441e75f5f978477
vallmar <markus_vall@hotmail.com>
2026-09-20 19:25:52 +0200

    Remove obsolete booking collision exception

--- src/CarRental.Application/Rentals/BookingNumberAlreadyExistsException.cs
+++
@@ -1,6 +0,0 @@
-namespace CarRental.Application.Rentals;
-
-public sealed class BookingNumberAlreadyExistsException(string bookingNumber)
-    : InvalidOperationException($"Booking number '{bookingNumber}' is already in use.")
-{
-}
